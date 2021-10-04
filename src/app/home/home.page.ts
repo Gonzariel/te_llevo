02b43779493d02a14811e7be8185368ff2a83339
Gonzariel <gonzarielred@gmail.com>
2021-10-04 00:41:16 -0300

    Entrega

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationExtras } from '@angular/router';
+import { ToastController} from '@ionic/angular';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  usuario: any = {
+    usser: '',
+    pass:''
+  };
 
-  constructor() {}
+  campoError:string ="";
 
+
+  constructor(public alertCtrl: AlertController,private router: Router,public toastController: ToastController) {}
+
+  ngOnInit(){
+
+  }
+
+
+  ingresar()
+  {
+    if(this.validarModelo(this.usuario))
+    {
+      
+      
+      if(this.usuario.usser == "Ariel" && this.usuario.pass =="1313"){
+        let navigationExtras: NavigationExtras = {
+          state: {
+           usuario: this.usuario
+          }};
+        console.log('usuario ' + this.usuario.usser + ' ' + this.usuario.pass);
+        this.router.navigate(['/inicio'],navigationExtras);
+      }
+      else{
+        this.mensajeToast(" Usuario o Contraseña Incorrecto");
+      }
+              
+    }
+
+  }
+
+  validarModelo(model:any)
+  {
+    for(var [key, value] of Object.entries(model))
+    {
+      console.log(key+" value: "+value);
+      if(value=="")
+      {
+        this.campoError = key;
+        return false;
+      }
+    }
+    return true;
+  }
+
+  async mensajeToast(message:string, duration?:number)
+  {
+    const toast = await this.toastController.create(
+      {
+        message :message,
+        duration: duration?duration:3000
+      }
+    );
+
+    toast.present();
+  }
+
+
+
+ 
 }
