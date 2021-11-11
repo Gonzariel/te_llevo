a4fa158d1a2fb1087638746e2de5d1e625f39a28
JUAN-CARLOS-QUINTEROS <ju.quinteroso@duocuc.cl>
2021-11-10 21:54:35 -0300

    VALIDACIONREGISTRO

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {Storage} from '@capacitor/storage';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationExtras } from '@angular/router';
+import { ToastController} from '@ionic/angular';
+
+
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
@@ -13,14 +19,60 @@ export class RegistroPage implements OnInit {
     email: "",
     pass: ""
   }
-  constructor() { }
-
-  ingresar(){
-    var infoJson =JSON.stringify(this.registro);
-    Storage.set({key: 'registro',value: infoJson});
-  }
+  campoError: string="";
+  constructor(public alertCtrl: AlertController,private router: Router,public toastController: ToastController) { }
 
   ngOnInit() {
   }
+
+  ingreso()
+  {
+    if(this.validarModelo(this.registro))
+    {
+      if(this.registro.nombre =="" && this.registro.pass=="")
+        var infoJson =JSON.stringify(this.registro);
+        Storage.set({key: 'registro',value: infoJson});
+        this.router.navigate(['/home']);
+    }
+    else{
+      this.mensajeToast("Porfavor llenar todos los campos");
+    }  
+  }
+
+ 
+
+  validarModelo(model: any)
+  {
+    for(var [key, value] of Object.entries(model))
+    {
+      console.log(key+" value:"+value);
+      if(value=="")
+      {
+      this.campoError = key;
+      return false;
+      }
+    }
+    return true;
+  }
+
+
+  async mensajeToast(message:string, duration?:number)
+  {
+    const toast = await this.toastController.create(
+      {
+        message :message,
+        duration: duration?duration:3000
+      }
+    );
+
+    toast.present();
+  }
+
+
+
+
+
+
+
 
 }
